9b520c13e788cc5addd8218d364e0d324f457cf8
nicolasjcortez <jordaocortez@gmail.com>
2022-10-16 17:46:39 -0300

    clean up

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -31,27 +31,6 @@ def create_user_link(db: Session, link: schemas.LinkCreate, user_id: int):
     return db_link
 
 
-##### other file ####
-
-# async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = schemas.TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
-#     user = get_user_by_username(db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
 def authenticate_user(db: Session, username: str, password: str):
     user = get_user_by_username(db, username)
     if not user:
@@ -59,22 +38,3 @@ def authenticate_user(db: Session, username: str, password: str):
     if not security.verify_password(password, user.hashed_password):
         return False
     return user
-
-# def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
-#     to_encode = data.copy()
-#     if expires_delta:
-#         expire = datetime.utcnow() + expires_delta
-#     else:
-#         expire = datetime.utcnow() + timedelta(minutes=15)
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
-
-
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-
-
-
--- backend/app/main.py
+++ backend/app/main.py
@@ -41,19 +41,6 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-# @app.get("/users/", response_model=list[schemas.User])
-# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     users = crud.get_users(db, skip=skip, limit=limit)
-#     return users
-
-
-# @app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
-
 @app.post("/token/", response_model=schemas.Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = crud.authenticate_user(db, form_data.username, form_data.password)
@@ -91,11 +78,6 @@ def get_links_for_user(
 
     pretty_print_response(response)
     return response
-
-# @app.get("/items/", response_model=list[schemas.Item])
-# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     items = crud.get_items(db, skip=skip, limit=limit)
-#     return items
 
 
 @app.post("/accounts")
@@ -193,23 +175,6 @@ def retrieve_income_report_data(incomes_req: schemas.LinkAndAccount):
     return response
 
 
-
-# @app.post("/balances")
-# def retrieve_balance(link: schemas.LinkWithDates):
-#     try:
-#         response = client.Balances.create(
-#             link=link.link,
-#             date_from=datetime.strftime(datetime.now() - timedelta(days=30), "%Y-%m-%d"),
-#             date_to=datetime.strftime(datetime.now(), "%Y-%m-%d"),
-#         )
-
-#     except RequestError as e:
-#         return format_error(e)
-
-#     pretty_print_response(response)
-#     return response
-
-
 @app.post("/owners")
 def retrieve_owners(link: schemas.LinkBase):
     try:
@@ -221,17 +186,6 @@ def retrieve_owners(link: schemas.LinkBase):
     pretty_print_response(response)
     return response
 
-
-# @app.post("/statements")
-# def retrieve_statements(link: schemas.LinkBase):
-#     try:
-#         response = client.Statements.create(link=link.link,)
-
-#     except RequestError as e:
-#         return format_error(e)
-
-#     pretty_print_response(response)
-#     return response
 
 def format_error(e):
     return {
